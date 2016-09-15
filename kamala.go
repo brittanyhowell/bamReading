3a9910458e1dbeb1f6092dc2aa18b9fb584878f4
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-15 17:26:50 +0930

    Dan helped. Scans chunks and assigns reads to chunks.

--- kamala.go
+++ kamala.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"os"
 
+	"github.com/biogo/biogo/io/featio"
 	"github.com/biogo/biogo/io/featio/bed"
 	"github.com/biogo/hts/bam"
+	"github.com/biogo/hts/sam"
 )
 
 func main() {
@@ -21,23 +23,6 @@ func main() {
 	bai, err := bam.ReadIndex(ind)
 	h := bai.NumRefs()
 	fmt.Printf("h is h: %v \n\n", h)
-
-	// Read location bed
-	loc, err := os.Open("tiny.bed")
-	if err != nil {
-		log.Printf("error: could not open %s to read %v", loc, err)
-	}
-	defer loc.Close()
-
-	lr, err := bed.NewReader(loc, 3)
-	if err != nil {
-		log.Printf("error in NewReader: %s, %v", loc, err)
-	}
-
-	l, err := lr.Read()
-	fmt.Printf("Locations: %v\n", l)
-
-	//bam.Chunks()
 
 	f, err := os.Open("sample5.bam")
 	if err != nil {
@@ -60,6 +45,48 @@ func main() {
 	hat := r.Cigar
 
 	fmt.Printf("hat: %v \n\n", hat)
+	refs := make(map[string]*sam.Reference, h)
+	for _, r := range br.Header().Refs() {
+		refs[r.Name()] = r
+	}
+
+	// Read location bed
+	loc, err := os.Open("tiny.bed")
+	if err != nil {
+		log.Printf("error: could not open %s to read %v", loc, err)
+	}
+	defer loc.Close()
+
+	lr, err := bed.NewReader(loc, 3)
+	if err != nil {
+		log.Printf("error in NewReader: %s, %v", loc, err)
+	}
+
+	fsc := featio.NewScanner(lr)
+	for fsc.Next() {
+		f := fsc.Feat().(*bed.Bed3)
+		fmt.Printf("NUM: %v. WORD. ref for %s is %#v\n", f, f.Chrom, refs[f.Chrom])
+
+		chunks, err := bai.Chunks(refs[f.Chrom], f.Start(), f.End())
+		fmt.Printf("%#v -> %+v %v\n", f, chunks, err)
+		i, err := bam.NewIterator(br, chunks)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i.Next() {
+			fmt.Println(i.Record())
+		}
+		err = i.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = fsc.Error()
+	if err != nil {
+		log.Fatalf("bed scan failed: %v", err)
+	}
+
+	//bam.Chunks()
 
 	//fmt.Printf("BR: %v \n\n", br)
 	//fmt.Printf("R: %v \n \n", r)
