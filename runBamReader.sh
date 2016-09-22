b5bbf0741af07df785798c9728507cf7fab1df35
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-22 18:38:17 +0930

    New add runs bamReader.go

---
+++ runBamReader.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Runs bamReader.go
+
+# go run bamReader.go -index=sample5Change.bam.bai -bam=sample5Change.bam -intervalsBed=tiny.bed -outPath=./Split/ -outName=smallSplit.bed
+
+WkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs
+index="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam.bai"
+bam="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam"
+
+intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/L1Location
+intervalsBed="ClusAllL1s.bed"
+
+outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs/Split/
+outFile="gapInReadShortWithDetail.bed"
+
+# WkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts
+# index="sample5Change.bam.bai"
+# bam="sample5Change.bam"
+
+# intDIR=${WkDIR}
+# intervalsBed="tiny.bed"
+
+# outDIR=${WkDIR}/Split/
+# outFile="splitShort.bed"
+
+
+go run bamReader.go -index=${WkDIR}/${index} -bam=${WkDIR}/${bam} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=${outFile}
