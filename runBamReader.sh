54d5cafb3bedde034c013380db8efab9346c7d1f
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-25 21:29:30 +0930

    add coverageSplitReads.R

--- runBamReader.sh
+++ runBamReader.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 # Runs bamReader.go
 
-# go run bamReader.go -index=sample5Change.bam.bai -bam=sample5Change.bam -intervalsBed=tiny.bed -outPath=./Split/ -outName=smallSplit.bed
 
-WkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs
-index="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam.bai"
-bam="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam"
+bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs
+# index="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam.bai"
+index="Mut-F2-Rep1_CGTACG_L007.tophat2_pe.mm10.bam.bai"
+# bam="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam"
+bam="Mut-F2-Rep1_CGTACG_L007.tophat2_pe.mm10.bam"
 
 intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/L1Location
 # intervalsBed="ClusActiveL1s.bed"
 intervalsBed="ClusAllL1s.bed"
 
-outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs/Split/
-outFile="gapInReadCigar.bed"
+outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split
+outFile="TopHatgapInReadCigar.bed"
 
-# WkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts
-# index="sample5Change.bam.bai"
-# bam="sample5Change.bam"
+## R file variables
+tableDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split
+gapTable="TopHatgapInReadCigar.bed"
+outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots
+pdfGAP="Tophat.pdf"
 
-# intDIR=${WkDIR}
-# intervalsBed="tiny.bed"
-
-# outDIR=${WkDIR}/Split/
-# outFile="splitShort.bed"
+# The go code
+# go run bamReader.go -index=${bamDIR}/${index} -bam=${bamDIR}/${bam} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=${outFile}
 
 
-go run bamReader.go -index=${WkDIR}/${index} -bam=${WkDIR}/${bam} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=${outFile}
+
+
+## Run the R script for plotting
+
+Rscript coverageSplitReads.R ${tableDIR}/${gapTable} ${outDIR}/${pdfGAP}
+# args 1: table name
+# args 2: name of PDF
