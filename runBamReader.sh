f3ddce75c11b77cb246c383ddd7646ee983f4c44
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-26 18:29:25 +0930

    added for loop sequentially takes in all bams in a file.

--- runBamReader.sh
+++ runBamReader.sh
@@ -2,33 +2,55 @@
 # Runs bamReader.go
 
 
-bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs
-# index="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam.bai"
-index="Mut-F2-Rep1_CGTACG_L007.tophat2_pe.mm10.bam.bai"
-# bam="Mut-F2-Rep1_CGTACG_L007.STAR.10.45.Aligned.sortedByCoord.out.bam"
-bam="Mut-F2-Rep1_CGTACG_L007.tophat2_pe.mm10.bam"
+bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs/transfer
+# index="Mut-F5-Rep1_ACAGTG_L008.STAR.10.45.bam.bai"
+# bam="Mut-F5-Rep1_ACAGTG_L008.STAR.10.45.bam"
 
 intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/L1Location
 # intervalsBed="ClusActiveL1s.bed"
 intervalsBed="ClusAllL1s.bed"
 
-outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split
-outFile="TopHatgapInReadCigar.bed"
+outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/Tues/
+# outFile="gapInRead_Mut_F5_Rep1_10_45.txt"
 
-## R file variables
-tableDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split
-gapTable="TopHatgapInReadCigar.bed"
-outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots
-pdfGAP="Tophat.pdf"
+# # testDIRs
+# bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams
 
+# # index="sample5Change.bam.bai"
+# # bam="sample5Change.bam"
+
+# intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams
+# # intervalsBed="ClusActiveL1s.bed"
+# intervalsBed="tiny.bed"
+
+# outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams
+# # outFile="gapInRead_testing.txt"
+
+
+cd ${bamDIR}
+bamFiles=$(ls *.bam.bai)
+
+for bam in $bamFiles ; do
+
+	bamRecord=${bam%.bai}
 # The go code
 # go run bamReader.go -index=${bamDIR}/${index} -bam=${bamDIR}/${bam} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=${outFile}
 
 
+ go run bamReader.go -index=${bamDIR}/${bam} -bam=${bamDIR}/${bamRecord} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=gapInRead_${bamRecord%.STAR.10.45.bam}.txt
+done 
 
 
-## Run the R script for plotting
 
-Rscript coverageSplitReads.R ${tableDIR}/${gapTable} ${outDIR}/${pdfGAP}
-# args 1: table name
-# args 2: name of PDF
+# ## R file variables
+# tableDIR=${outDIR}
+# gapTable=${outFile}
+# plotDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots
+# pdfGAP="STAR_F5Rep1_10_45.pdf"
+# plotName="STAR-F5_Rep1_10_45"
+# ## Run the R script for plotting
+
+# Rscript coverageSplitReads.R ${tableDIR}/${gapTable} ${plotDIR}/${pdfGAP} ${plotName}
+# # args 1: table name
+# # args 2: name of PDF
+# # args 3: title on plot
