7f8b15f234ad2c6229dd3a618f80608e83acdcc8
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-01 14:02:12 +0930

    Edited for mouse detection

--- runBamReader.sh
+++ runBamReader.sh
@@ -1,70 +1,74 @@
 #!/bin/bash
 # Runs bamReader.go
 
+scriptDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/
 
-bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs/transfer
-# index="Mut-F5-Rep1_ACAGTG_L008.STAR.10.45.bam.bai"
-# bam="Mut-F5-Rep1_ACAGTG_L008.STAR.10.45.bam"
+bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/BAMs/transfer/
+# # index="Mut-F5-Rep1_ACAGTG_L008.STAR.10.45.bam.bai"
+# # bam="Mut-F5-Rep1_ACAGTG_L008.STAR.10.45.bam"
 
-intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/L1Location
-# intervalsBed="ClusActiveL1s.bed"
-intervalsBed="ClusAllL1s.bed"
 
-outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/limitedGap/
-# outFile="gapInRead_Mut_F5_Rep1_10_45.txt"
+intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/L1Location/
+intervalsBed="ClusActiveL1s.bed"
+# intervalsBed="ClusAllL1s.bed"
 
-# # testDIRs
+refGenDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/genomeAssembliess
+refGen="mm10.fa"
+
+outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/mouse/
+outPrefix=gapInRead${bamRecord%.STAR.10.45.bam}
+
+
+
+# testDIRs
 # bamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams
-
-# # index="sample5Change.bam.bai"
-# # bam="sample5Change.bam"
-
 # intDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams
-# # intervalsBed="ClusActiveL1s.bed"
 # intervalsBed="tiny.bed"
+# outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams/
+# outPrefix=gapInRead${bamRecord%.STAR.10.45.bam}
+# refGenDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/genomeAssemblies
+# refGen="mm10.small"
 
-# outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Scripts/sampleBams
-# # outFile="gapInRead_testing.txt"
-
+cp bamReader.go ${bamDIR}
 
 cd ${bamDIR}
 bamFiles=$(ls *.bam.bai)
 
 for bam in $bamFiles ; do
 	bamRecord=${bam%.bai}
-# The go code
-# go run bamReader.go -index=${bamDIR}/${index} -bam=${bamDIR}/${bam} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=${outFile}
 
-
- go run bamReader.go -index=${bamDIR}/${bam} -bam=${bamDIR}/${bamRecord} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=gapInRead_${bamRecord%.STAR.10.45.bam}.txt
+ 	go run bamReader.go -index=${bamDIR}/${bam} -bam=${bamDIR}/${bamRecord} -intervalsBed=${intDIR}/${intervalsBed} -outPath=${outDIR} -outName=${outPrefix}.txt -seqOutName=${outPrefix}_FullIntron.txt -refGen=${refGenDIR}/${refGen} -logo5Name=${outPrefix}_5SJ.txt -logo3Name=${outPrefix}_3SJ.txt
+ 
 done 
-
-echo "finished making tables"
-
-## R file variables
-tableDIR=${outDIR}
-plotDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots/full
+rm bamReader.go
 
 
 
-cd ${outDIR}
+echo "I finished making tables :)"
 
-for gap in *.txt; do 
+# ## R file variables
+# tableDIR=${outDIR}
+# plotDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots/full
 
-	gapTable=${gap}
-	makeName=$(echo $gap | sed 's/_/	/g' | awk '{print $2}')
-	pdfGAP="${makeName}.pdf"
-	plotName="STAR-${makeName}"
-	## Run the R script for plotting
 
-# echo ${gapTable}
-# echo ${makeName}
-# echo ${pdfGAP}
-# echo ${plotName}
-# echo .
-	Rscript coverageSplitReads.R ${tableDIR}/${gapTable} ${plotDIR}/${pdfGAP} ${plotName}
-	# args 1: table name
-	# args 2: name of PDF
-	# args 3: title on plot
+# cd ${outDIR}
 
-done
+# for gap in *.txt; do 
+
+# 	gapTable=${gap}
+# 	makeName=$(echo $gap | sed 's/_/	/g' | awk '{print $2}')
+# 	pdfGAP="${makeName}.pdf"
+# 	plotName="STAR-${makeName}"
+# 	## Run the R script for plotting
+
+# # echo ${gapTable}
+# # echo ${makeName}
+# # echo ${pdfGAP}
+# # echo ${plotName}
+# # echo .
+# 	# Rscript coverageSplitReads.R ${tableDIR}/${gapTable} ${plotDIR}/${pdfGAP} ${plotName}
+# 	# args 1: table name
+# 	# args 2: name of PDF
+# 	# args 3: title on plot
+
+# done
