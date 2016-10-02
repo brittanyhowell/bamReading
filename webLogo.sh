ad517ad95e4c3985b00e01ffba20bb2379763b12
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-02 18:06:32 +1030

    add argument variables and change some inputs

--- webLogo.sh
+++ webLogo.sh
@@ -3,18 +3,35 @@
 
 
 
-wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/runWithGenomeSpliceSites/Mouse/
-plotDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots/webLogo
-inTable=${wkDIR}/Three
+wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/runWithGenomeSpliceSites/Human/
+inTable=${wkDIR}/Five
+
+site="Human-Five"
+
+plotDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/plots/webLogo/${site}
 
 
-cd ${wkDIR}/Five
+if [ -d $plotDIR ]; then
+	rm -r $plotDIR 
+	mkdir $plotDIR
+	echo "plot folder exists... replacing" 
+else 
+	echo "creating plot folder" 
+	mkdir $plotDIR
+fi 
+
+
+
+cd ${inTable}
 for table in *.txt ; do 
 
-fileName="${table%.txt}"
-echo "making for ${fileName}"
+	fileName="${table%.txt}"
 
-	weblogo --number-interval 2 --aspect-ratio 4 -y "" -W 20 -c classic --format PDF -f ${table} -o ${plotDIR}/${fileName}.pdf
+	noGAP="$(echo $fileName | sed 's/gapInRead//g')"
+	echo $noGAP
+
+
+	weblogo --number-interval 2 --aspect-ratio 4 -y "" --size large --title "$noGAP" --annotate '-3,-2,-1,+1,+2,+3' -W 30  -c classic --format PDF -f ${table} -U probability -o ${plotDIR}/${fileName}.pdf 
 done
 
 echo "complete"
