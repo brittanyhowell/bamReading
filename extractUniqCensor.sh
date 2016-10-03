d750523fbfaca023f56d3265be19a3fd32d944d1
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-03 15:43:39 +1030

    New script Some commands to extract only the big hits from censor output

---
+++ extractUniqCensor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Some commands to get just the unique censor results from the map file
+
+
+# First, find the L1s that are broken up and found in multiple sequences: 
+awk '{print $1}' mouseUniqueL1.fasta.map | sort | uniq -c | sort
+
+# Identify those >1, and determine which bits to keep.
+# All of the breaks of hits were less than 555bp. To confirm them, print only those smaller than 555bp
+awk '{if ($3-$2 < 555) print $0}' mouseUniqueL1.fasta.map
+
+
+# If they are the correct ones, flip the sign to a ">" sign and it will print just the good ones. 
+awk '{if ($3-$2 > 555) print $0}' mouseUniqueL1.fasta.map
