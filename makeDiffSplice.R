d583586d572a2d4624d8209f13ebd71efd261e6d
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-05 12:06:48 +1030

    change axes labels

--- makeDiffSplice.R
+++ makeDiffSplice.R
@@ -6,5 +6,5 @@ events <- read.table("AllComp.txt")
 colnames(events) <- c("All", "Either", "Both", "Stringent")
 
   colours <- c("salmon1" ,"salmon1" ,"salmon1" ,"palevioletred" ,"palevioletred" ,"palevioletred" ,"orchid4","orchid4","orchid4", "slateblue" ,"slateblue" ,"slateblue" ,"cornflowerblue", "cornflowerblue", "cornflowerblue", "aquamarine", "aquamarine", "aquamarine", "darkolivegreen1", "darkolivegreen1", "darkolivegreen1", "chartreuse4", "chartreuse4", "chartreuse4", "darkgreen", "darkgreen", "darkgreen")
-  barplot(subset, beside=T, names.arg = colnames(events), col = colours, ylab = "Number of candidate splices")
+  barplot(subset, beside=T, names.arg = colnames(events), col = colours, ylab = "Number of candidate splice events")
   
