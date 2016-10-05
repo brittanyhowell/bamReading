362d8c291ef29432ba5124166b1d1b09ccc00fd4
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-05 12:05:43 +1030

    Plots number of splice events colour is per sample.

---
+++ makeDiffSplice.R
@@ -0,0 +1,10 @@
+
+
+setwd("~/Documents/University/Honours_2016/Project/bamReading/Split/runWithGenomeSpliceSites/Human/Compile")
+events <- read.table("AllComp.txt")
+
+colnames(events) <- c("All", "Either", "Both", "Stringent")
+
+  colours <- c("salmon1" ,"salmon1" ,"salmon1" ,"palevioletred" ,"palevioletred" ,"palevioletred" ,"orchid4","orchid4","orchid4", "slateblue" ,"slateblue" ,"slateblue" ,"cornflowerblue", "cornflowerblue", "cornflowerblue", "aquamarine", "aquamarine", "aquamarine", "darkolivegreen1", "darkolivegreen1", "darkolivegreen1", "chartreuse4", "chartreuse4", "chartreuse4", "darkgreen", "darkgreen", "darkgreen")
+  barplot(subset, beside=T, names.arg = colnames(events), col = colours, ylab = "Number of candidate splices")
+  
