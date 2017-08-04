3b2de50b6c9e4698edf85fd2b30b328aea89a04e
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2017-08-04 13:38:02 +0930

    changed call

--- phoenixSubmitBamReader.sh
+++ phoenixSubmitBamReader.sh
@@ -1,6 +1,6 @@
 ##submits many bamReader.go scripts
 
-bamDIR=/data/rc003/Brittany/humanStrictAlignment/testNormalise
+bamDIR=/data/rc003/Brittany/Alignment/mouseBAM/normaliseRunTest
 scriptDIR=/data/rc003/Brittany/Scripts
 
 cd ${scriptDIR}
@@ -13,5 +13,6 @@ bamList=$(ls *.bam.bai)
 for bam in $bamList ; do
 
 	echo "Running for $bam"
-				scriptDIR=/data/rc003/Brittany/Scripts  bamDIR=/data/rc003/Brittany/humanStrictAlignment/testNormalise  intDIR=/data/rc003/Brittany/Data/L1Location  refGenDIR=/data/rc003/Brittany/Data/genomes outDIR=~/humNorm2/ dataDIR=/data/rc003/Brittany/Data/ sbatch runBamReader.sh ${bam}
+				# scriptDIR=/data/rc003/Brittany/Scripts  bamDIR=/data/rc003/Brittany/humanStrictAlignment/testNormalise  intDIR=/data/rc003/Brittany/Data/L1Location  refGenDIR=/data/rc003/Brittany/Data/genomes outDIR=~/humNorm2/ dataDIR=/data/rc003/Brittany/Data/ sbatch runBamReader.sh ${bam}
+				scriptDIR=/data/rc003/Brittany/Scripts/  bamDIR=/data/rc003/Brittany/Alignment/mouseBAM/normaliseRunTest intDIR=/data/rc003/Brittany/Data/L1Location/  dataDIR=/data/rc003/Brittany/Data/sjMaps/ refGenDIR=/data/rc003/Brittany/Data/genomes/ outDIR=~/musNorm/ sbatch runBamReader.sh ${bam}
 done
