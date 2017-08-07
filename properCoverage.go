955ba8370b09cee4e2caddded0391c686bd23f09
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2017-08-07 13:39:45 +0930

    can almost digest. Some issues

--- properCoverage.go
+++ properCoverage.go
@@ -1,123 +1,103 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"strings"
+)
 
-	"github.com/biogo/hts/sam"
+type reads struct {
+	chr    string
+	gStart int
+	// gEnd   int
+	// lStart int
+	// lEnd   int
+	// rStart int
+	// rEnd   int
+	// Cigar  sam.Cigar
+}
+
+var (
+	readFile string
 )
 
 func main() {
 
-	for _, co := range r.Cigar {
+	flag.StringVar(&readFile, "readFile", "", "File with read data")
+	flag.Parse()
 
-		pos := r.Pos
-		t := co.Type()
-		con := t.Consumes()
-		gapLen = co.Len() * con.Reference
-
-		if con.Query == con.Reference {
-			o := min(pos+gapLen, r.End()) - max(pos, r.Start())
-			if o > 0 {
-				overlap += o
-				readOverlap += o
-			}
-		}
-		overlap += extra
-		extra = 0
-
-		startInL1 = r.Start() - f.Start()
-		endInL1 = r.End() - f.Start()
-		startGap = startInL1 + overlap
-		endGap = startInL1 + overlap + gapLen
-
-		switch co.Type() {
-		case sam.CigarSkipped, sam.CigarInsertion:
-
-			fSplice = true
-			countSplice = true
-			if gapLen > 4 {
-
-				// genomic position of gap in read
-				genStartGap := startGap + f.Start()
-				genEndGap := endGap + f.Start()
-
-				// genomic position of splice junction
-				genStartSJ := genStartGap - 2
-				genEndSJ := genEndGap - 2
-
-				// Reading the nucleotides of the SJ in string form
-				var buffer5 bytes.Buffer
-				for i := genStartSJ; i < genStartSJ+4; i++ {
-					buffer5.WriteString(string(AllSeqs[f.Chrom].At(i).L))
-				}
-				sFiveSJ := strings.ToUpper(buffer5.String())
-
-				var buffer3 bytes.Buffer
-				for i := genEndSJ; i < genEndSJ+2; i++ {
-					buffer3.WriteString(string(AllSeqs[f.Chrom].At(i).L))
-				}
-				sThreeSJ := strings.ToUpper(buffer3.String())
-
-				// Look SJ nucs up in maps
-				fiveSJ := All5SJ[sFiveSJ]
-				threeSJ := All3SJ[sThreeSJ]
-
-				fmt.Println("The 5' class:", All5SJ[sFiveSJ], "Proof:", sFiveSJ)
-				fmt.Println("The 3' class:", All3SJ[sThreeSJ], "Proof:", sThreeSJ)
-
-				fmt.Fprintf(out, "%v \t%v \t %v \t %v \t %v \t %v \t %v \t %v \t %v \t %v \t %v \t %v \t %v\n",
-					r.Name,    // read name
-					f.Chrom,   // chromosome of L1
-					f.Start(), // L1 genomic start
-					f.End(),   // L1 genomic end
-					startGap,  // start position of gap relative to L1
-					endGap,    // end position of gap relative to L1
-					fiveSJ,    // Class of 5' SJ
-					threeSJ,   // Class of 3' SJ
-					sFiveSJ,   // 5' nucs
-					sThreeSJ,  // 3' nucs
-					gapLen,    // length of gap
-					r.Cigar,   // cigar string of read
-					r.Flags,   // flags relative to read
-				)
-
-				// Include only if there is need for an intron
-				if seqOutName != "" {
-					nucs := AllSeqs[f.Chrom].Slice()
-
-					fmt.Fprintf(seqOut, "%v \t%v \t %v \t %v \t %v \t %v \n",
-						f.Chrom,   // chromosome of L1
-						f.Start(), // L1 genomic start
-						f.End(),   // L1 genomic end
-						startGap,  // start position of gap relative to L1
-						endGap,    // end position of gap relative to L1
-						nucs.Slice(genStartGap-3, genEndGap+3), //
-					)
-				}
-
-				// splice logo fasta files
-				if logo5Name != "" {
-					fmt.Fprintf(logo5out, ">Logo-5'%v:%v-%v\n%v\n",
-						f.Chrom,  // chromosome name
-						startGap, // start position of gap relative to L1
-						endGap,   // end position of gap relative to L1
-						sFiveSJ,  // letters at begin of splice
-					)
-				}
-
-				if logo3Name != "" {
-					fmt.Fprintf(logo3out, ">Logo-3'%v:%v-%v\n%v\n",
-						f.Chrom,  // chromosome name
-						startGap, // start position of gap relative to L1
-						endGap,   // end position of gap relative to L1
-						sThreeSJ, // letters at begin of splice
-					)
-				}
-			}
-			extra = gapLen // adds to overlap
-		}
+	f, err := os.Open(readFile)
+	if err != nil {
+		log.Printf("error: could not open %s to read %v", f, err)
 	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	// sc.Split(bufio.ScanLines)
+	i := 0
+
+	// block := []string{}
+
+	for sc.Scan() {
+
+		line := sc.Text()
+		line = strings.TrimSpace(line)
+		words := strings.Split(line, " ")
+
+		word, err := strconv.Atoi(words[1])
+		if err != nil {
+			os.Exit(1)
+
+		}
+
+		fmt.Printf("words: %v  (%T)\n word: %v (%T)\n", words[1], words[1], word, word)
+		dat := reads{chr: words[0], gStart: word}
+		// block = append(block, line)
+		i++
+		// fmt.Println("So far:", block, i)
+		fmt.Println(dat)
+	}
+
+	// fmt.Println("a block: ", block)
+
+	// scS := seqio.NewScanner(r)
+	// for scS.Next() {
+	// 	v = append(v, scS.Seq())
+	// }
+	// if sc.Error() != nil {
+	// 	log.Fatalf("failed to read sequences: %v", sc.Error())
+	// }
+
+	// for _, co := range r.Cigar {
+
+	// 	pos := r.Pos
+	// 	t := co.Type()
+	// 	con := t.Consumes()
+	// 	gapLen = co.Len() * con.Reference
+
+	// 	if con.Query == con.Reference {
+	// 		o := min(pos+gapLen, r.End()) - max(pos, r.Start())
+	// 		if o > 0 {
+	// 			overlap += o
+	// 		}
+	// 	}
+	// 	overlap += extra
+	// 	extra = 0
+
+	// 	startInL1 = r.Start() - f.Start()
+	// 	endInL1 = r.End() - f.Start()
+	// 	startGap = startInL1 + overlap
+	// 	endGap = startInL1 + overlap + gapLen
+
+	// 	switch co.Type() {
+	// 	case sam.CigarSkipped, sam.CigarInsertion:
+
+	// 		extra = gapLen // adds to overlap
+	// 	}
+	// }
 
 }
