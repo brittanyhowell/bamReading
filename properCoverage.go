78c74507f83de3a5804c0f8bf25afb40a4369f19
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2017-08-09 16:25:20 +0930

    processes somewhat

--- properCoverage.go
+++ properCoverage.go
@@ -2,33 +2,58 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 	"strconv"
-	"strings"
+	"unsafe"
+
+	"github.com/biogo/hts/sam"
 )
 
 type reads struct {
-	chr    string
-	gStart int
-	// gEnd   int
-	// lStart int
-	// lEnd   int
-	// rStart int
-	// rEnd   int
-	// Cigar  sam.Cigar
+	Chrom      string
+	ChromStart int
+	ChromEnd   int
+	L1Start    int
+	L1End      int
+	ReadStart  int
+	ReadEnd    int
+	Cigar      sam.Cigar
 }
+
+const (
+	ChromField = iota
+	ChromStartField
+	ChromEndField
+	L1StartField
+	L1EndField
+	ReadStartField
+	ReadEndField
+	CigarField
+)
 
 var (
 	readFile string
+	out      string
 )
 
 func main() {
 
 	flag.StringVar(&readFile, "readFile", "", "File with read data")
+	flag.StringVar(&out, "outFile", "", "cov interval file")
 	flag.Parse()
+
+	o := fmt.Sprintf("%v", out)
+	oFile, err := os.Create(o)
+	if err != nil {
+		log.Fatalf("failed to create output file %s: %v", o, err)
+	}
+	defer oFile.Close()
 
 	f, err := os.Open(readFile)
 	if err != nil {
@@ -37,67 +62,140 @@ func main() {
 	defer f.Close()
 
 	sc := bufio.NewScanner(f)
-	// sc.Split(bufio.ScanLines)
-	i := 0
-
-	// block := []string{}
+	var num int
 
 	for sc.Scan() {
 
-		line := sc.Text()
-		line = strings.TrimSpace(line)
-		words := strings.Split(line, " ")
+		line := sc.Bytes()
 
-		word, err := strconv.Atoi(words[1])
 		if err != nil {
-			os.Exit(1)
+			log.Fatalf("failed with %v\n", err)
+		}
+		num++
+		c, err := parseReadFile(line)
 
+		if err != nil {
+			if err, ok := err.(*csv.ParseError); ok {
+				err.Line = num
+				log.Fatalf("failed with %v\n", err)
+			}
+			fmt.Errorf("%v at line %d", err, num)
 		}
 
-		fmt.Printf("words: %v  (%T)\n word: %v (%T)\n", words[1], words[1], word, word)
-		dat := reads{chr: words[0], gStart: word}
-		// block = append(block, line)
-		i++
-		// fmt.Println("So far:", block, i)
-		fmt.Println(dat)
+		fmt.Println("a line:", c)
+
+		// Get to the consume
+		var (
+			// startInL1 int
+			// endInL1   int
+			// startGap  int
+			// endGap    int
+
+			intLen int
+			// overlap     int
+			// extra       int
+			// readOverlap int
+		)
+		for _, co := range c.Cigar {
+			pos := c.ReadStart
+
+			t := co.Type()
+			con := t.Consumes()
+			intLen = co.Len() * con.Reference
+
+			if con.Query == con.Reference {
+				// o := min(pos+intLen, r.End()) - max(pos, r.Start())
+				// if o > 0 {
+				// overlap += o
+				// }
+				fmt.Println("con stuff:", con.Query, con.Reference)
+			}
+			// 	overlap += extra
+			// 	extra = 0
+			// fmt.Fprintf(oFile, "%v\t%v", start, end?)
+			fmt.Println(intLen, pos)
+		}
+
+		// block = append(block, c)
+
 	}
 
-	// fmt.Println("a block: ", block)
+}
 
-	// scS := seqio.NewScanner(r)
-	// for scS.Next() {
-	// 	v = append(v, scS.Seq())
-	// }
-	// if sc.Error() != nil {
-	// 	log.Fatalf("failed to read sequences: %v", sc.Error())
-	// }
+// BED format reader type.
+type Reader struct {
+	r       *bufio.Reader
+	BedType int
+	line    int
+}
 
-	// for _, co := range r.Cigar {
+func parseReadFile(line []byte) (b reads, err error) {
+	const n = 8
+	defer handlePanic(b, &err)
+	c := bytes.Fields(line)
+	if len(c) < n {
+		log.Fatalf("wrong number of columns. Have %v, need %v \n", len(c), n)
+		return
+	}
 
-	// 	pos := r.Pos
-	// 	t := co.Type()
-	// 	con := t.Consumes()
-	// 	gapLen = co.Len() * con.Reference
+	b = reads{
+		Chrom:      string(c[ChromField]),
+		ChromStart: mustAtoi(c[ChromStartField], ChromStartField),
+		ChromEnd:   mustAtoi(c[ChromEndField], ChromEndField),
+		L1Start:    mustAtoi(c[L1StartField], L1StartField),
+		L1End:      mustAtoi(c[L1EndField], L1EndField),
+		ReadStart:  mustAtoi(c[ReadStartField], ReadStartField),
+		ReadEnd:    mustAtoi(c[ReadEndField], ReadEndField),
+		Cigar:      mustCigar(c[CigarField], CigarField),
+	}
+	return
+}
 
-	// 	if con.Query == con.Reference {
-	// 		o := min(pos+gapLen, r.End()) - max(pos, r.Start())
-	// 		if o > 0 {
-	// 			overlap += o
-	// 		}
-	// 	}
-	// 	overlap += extra
-	// 	extra = 0
+func mustCigar(f []byte, column int) sam.Cigar {
+	i, err := sam.ParseCigar(f)
+	if err != nil {
+		panic(&csv.ParseError{Column: column, Err: err})
+	}
+	return i
+}
 
-	// 	startInL1 = r.Start() - f.Start()
-	// 	endInL1 = r.End() - f.Start()
-	// 	startGap = startInL1 + overlap
-	// 	endGap = startInL1 + overlap + gapLen
+func mustAtoi(f []byte, column int) int {
+	i, err := strconv.ParseInt(unsafeString(f), 0, 0)
+	if err != nil {
+		panic(&csv.ParseError{Column: column, Err: err})
+	}
+	return int(i)
+}
 
-	// 	switch co.Type() {
-	// 	case sam.CigarSkipped, sam.CigarInsertion:
+func handlePanic(f reads, err *error) {
+	r := recover()
+	if r != nil {
+		e, ok := r.(error)
+		if !ok {
+			panic(r)
+		}
+		if _, ok = r.(runtime.Error); ok {
+			panic(r)
+		}
+		*err = e
+	}
+}
 
-	// 		extra = gapLen // adds to overlap
-	// 	}
-	// }
+// This function cannot be used to create strings that are expected to persist.
+func unsafeString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
 
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
